decf459d4fab51caeab3ac19170eee40f5ca6193
Basterg <SitxBasterg@inbox.ru>
2023-09-14 19:17:40 +0400

    layoutHeaderCore

--- src/components/screens/about-us/about-us.component.js
+++ src/components/screens/about-us/about-us.component.js
@@ -2,8 +2,9 @@ import { BaseScreen } from "@/core/component/base-screen.component";
 
 export class AboutUs extends BaseScreen {
   constructor() {
-    super({ title: "About Us" });
+    super({ title: "About us" });
   }
+
   render() {
     return "<p>About us</p>";
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import "@/styles/global.scss";
-import { Router } from "./core/router/router";
+import '@/styles/global.scss'
 
-new Router();
+import { Router } from './core/router/router'
+
+new Router()
